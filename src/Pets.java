d76749ded2d09ae4ee2a9be1e021f04ea6d00365
Paige Hetherington <herbmama@gmail.com>
2016-02-01 13:16:35 -0500

    pets class

---
+++ src/Pets.java
@@ -0,0 +1,12 @@
+/**
+ * Created by vajrayogini on 2/1/16.
+ */
+public class Pets {
+    public String breed;
+    public int age;
+    public boolean isFixed;
+    public boolean isMale = true;
+    public String color;
+    
+
+}
