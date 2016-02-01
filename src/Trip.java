777b51b39bd28ba9c5106c7b8e3de0a599795bad
Paige Hetherington <herbmama@gmail.com>
2016-02-01 12:57:43 -0500

    trip class

---
+++ src/Trip.java
@@ -0,0 +1,9 @@
+/**
+ * Created by vajrayogini on 2/1/16.
+ */
+public class Trip {
+    public String country;
+    public int numberOfDays;
+    public boolean haveVisa = true;
+    public boolean haveVaccination = true;
+}
