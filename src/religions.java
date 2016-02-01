608a5be71a89bf4490361b1e15975b0264e75050
Paige Hetherington <herbmama@gmail.com>
2016-02-01 13:12:13 -0500

    religions class

---
+++ src/religions.java
@@ -0,0 +1,11 @@
+/**
+ * Created by vajrayogini on 2/1/16.
+ */
+public class religions {
+    public int age;
+    public String name;
+    public boolean isMonotheistic = true;
+    public String leaderName;
+    public String countryOfOrigin;
+
+}
