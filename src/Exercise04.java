8f9ccd791800aaedcee86f50c79b1a86529ea00d
Paige Hetherington <herbmama@gmail.com>
2016-02-22 12:23:10 -0500

    added Exercise04

--- src/Exercise04.java
+++ src/Exercise04.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 /**
  * Created by vajrayogini on 2/22/16.
@@ -8,12 +9,13 @@ public class Exercise04 {
         String sentence = "To be or not to be, that is the question.";
         // to be or not that is the question - create string with duplicates removed
         sentence = sentence.toLowerCase();
+        sentence = sentence.replace(",", "").replace(".", "");
         String[] words = sentence.split(" ");
         System.out.println(words);
-        HashMap<String, Integer> frequencies = new HashMap<>();
+        LinkedHashMap<String, Integer> frequencies = new LinkedHashMap<>();
         for (String word : words) {
             Integer count = frequencies.get(word);
-            if (count > 1) {
+            if (count == null) {
                 frequencies.put(word, 1);
             } //else {
                //frequencies.remove(word, 1);
